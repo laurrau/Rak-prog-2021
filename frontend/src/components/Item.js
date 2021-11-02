d53bd20cdbcfcd4a0c5edd7d701756a698448f9a
laurrau <laurir@tlu.ee>
2021-11-02 12:43:12 +0200

    react start

---
+++ frontend/src/components/Item.js
@@ -0,0 +1,12 @@
+function Item() {
+	//komponendi nimi
+	return (
+		<div>
+			<div className="itemName">Ese nr1</div>
+			<div className="itemPrice">100</div>
+			<div className="itemCategory">mobiles</div>
+		</div>
+	);
+}
+
+export default Item;
